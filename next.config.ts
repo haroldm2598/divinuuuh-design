f874c4ee71a671d175eaa8a935ada9b5ee024a59
rold dev <cvuyante24@gmail.com>
2026-08-26 21:41:34 +0800

    feat: clerk and supabase connected

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
+    allowedDevOrigins: ["astound-antler-flattered.ngrok-free.dev"],
     images: {
         remotePatterns: [
             {
